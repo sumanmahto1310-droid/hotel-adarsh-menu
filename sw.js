1361b31a6c87e92bf3676e7092f35d7fd3dc37ff
sumanmahto1310-droid <sumanmahto1310@gmail.com>
2026-08-10 23:08:52 +0530

    Create sw.js
    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "hotel-adarsh-v1";
+
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/images/hotel-adarsh-pwa-192.png",
+  "/images/hotel-adarsh-pwa-512.png",
+  "/images/hotel-adarsh-logo.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
